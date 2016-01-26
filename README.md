f92fae7b1937c5362c49ee00df8a51d0eb9d71cb
Jasper Van Proeyen <trianglejuice@mac.com>
2016-01-26 12:26:14 +0100

    * Fixed Grunt errors * Implemented Tink coding guidelines * Cleaned up a bit

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Tink skeleton Angular directive
+# Tink GIS Angular directive
 
 v1.0.0
 
 ## What is this repository for?
 
-The Tink Angular skeleton provides a scaffold for a directive or service that can easily work with Tink.
+The Tink Angular GIS component provides a simple way to integrate maps into your app.
 
 Tink is an in-house developed easy-to-use front end framework for quick prototyping and simple deployment of all kinds of websites and apps, keeping a uniform and consistent look and feel.
 
@@ -18,9 +18,9 @@ Tink is an in-house developed easy-to-use front end framework for quick prototyp
 ### Install
 
 1. Go to the root of your project and type the following command in your terminal:
-   `bower install tink-back-to-top-angular --save`
+   `bower install tink-gis-angular --save`
 
-2. Include `dist/tink-back-to-top-angular.js` and its necessary dependencies in your project.
+2. Include `dist/tink-gis-angular.js` and its necessary dependencies in your project.
 
 3. On http://tink.digipolis.be you will find all necessary documentation.
 
@@ -33,6 +33,7 @@ Tink is an in-house developed easy-to-use front end framework for quick prototyp
 
 ## Who do I talk to?
 
+* Maxim Crabbé - maxim.crabbe@digipolis.be - Lead development
 * Jasper Van Proeyen - jasper.vanproeyen@digipolis.be - Lead front-end
 * Tom Wuyts - tom.wuyts@digipolis.be - Lead UX
 * [The hand](https://www.youtube.com/watch?v=_O-QqC9yM28)
--- src/scripts/directives/tinkmap.js
+++ src/scripts/directives/tinkmap.js
@@ -1,57 +1,54 @@
 'use strict';
 (function (module) {
-    try {
-        module = angular.module('tink.gis.angular');
-    } catch (e) {
-        module = angular.module('tink.gis.angular', ['leaflet-directive']);
-    }
-    module.directive('tinkMap', function () {
-        return {
-            templateUrl: "templates/tinkmaptemplate.html",
-            scope: { 
-                layers: "=",
-                center:  "="
-              },
-            controller: function ($scope) {
-                 var ourlayers = $scope.layers;
-             
+  try {
+    module = angular.module('tink.gis');
+  } catch (e) {
+    module = angular.module('tink.gis', ['leaflet-directive']);
+  }
+  module.directive('tinkMap',[function() {
+    return {
+      templateUrl: 'templates/tinkmaptemplate.html',
+      scope: {
+        layers: '=',
+        center: '='
+      },
+      controller: function ($scope) {
 
-                $scope.changeLayer = function (layername) {
-                    $scope.layers = ourlayers[layername];
-                };
-                angular.extend($scope, {
-                    defaults: {
-                        zoomControl: false
-                    },
-                    controls: {
-                        scale: {
-                            imperial: false
-                        }
-                    }
-                });
-                if ($scope.center == undefined) {
-                    angular.extend($scope, {
-                        center: $scope.center
-                    });
+        var ourlayers = $scope.layers;
+        // console.log(ourlayers);
 
-                }
-                if ($scope.layers == undefined || $scope.layers.baselayers == undefined) {
-                    angular.extend($scope, {
-                        layers: {
-                            baselayers: {
-                                kaart: ourlayers.kaart,
-                                luchtfoto: ourlayers.luchtfoto
-                            },
-                            
-                        }
-                        
-                    });
-                }
+        $scope.changeLayer = function (layername) {
+          $scope.layers = ourlayers[layername];
+        };
 
+        angular.extend($scope, {
+          defaults: {
+            zoomControl: false
+          },
+          controls: {
+            scale: {
+              imperial: false
             }
+          }
+        });
+
+        if ($scope.center === undefined) {
+          angular.extend($scope, {
+            center: $scope.center
+          });
         }
-    })
-})
-    ();
 
-
+        if ($scope.layers === undefined || $scope.layers.baselayers === undefined) {
+          angular.extend($scope, {
+            layers: {
+              baselayers: {
+                kaart: ourlayers.kaart,
+                luchtfoto: ourlayers.luchtfoto
+              },
+            }
+          });
+        }
+      }
+    };
+  }]);
+})();
