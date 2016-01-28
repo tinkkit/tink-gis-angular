0a4ab5d314935c453a36024a7dfe23cac838098c
maxim <crabbemaxim@gmail.com>
2016-01-28 10:04:45 +0100

    Minor fixes to index html so it will work standalone.

--- dist/tink-gis-angular.js
+++ dist/tink-gis-angular.js
@@ -7,19 +7,52 @@
     }
     module.directive('tinkMap', function () {
         return {
-            templateUrl: "templates/tinkmaptemplate.html",
-            scope: { 
-                layers: "=",
-                center:  "="
-              },
+            templateUrl: 'templates/tinkmaptemplate.html',
+            scope: {
+                parlayers: '=',
+                parcenter: '='
+            },
             controller: function ($scope) {
-                 var ourlayers = $scope.layers;
-             
+                function clone(obj) {
+                    var copy;
 
-                $scope.changeLayer = function (layername) {
-                    $scope.layers = ourlayers[layername];
-                };
+                    // Handle the 3 simple types, and null or undefined
+                    if (null == obj || "object" != typeof obj) return obj;
+
+                    // Handle Date
+                    if (obj instanceof Date) {
+                        copy = new Date();
+                        copy.setTime(obj.getTime());
+                        return copy;
+                    }
+
+                    // Handle Array
+                    if (obj instanceof Array) {
+                        copy = [];
+                        for (var i = 0, len = obj.length; i < len; i++) {
+                            copy[i] = clone(obj[i]);
+                        }
+                        return copy;
+                    }
+
+                    // Handle Object
+                    if (obj instanceof Object) {
+                        copy = {};
+                        for (var attr in obj) {
+                            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+                        }
+                        return copy;
+                    }
+
+                    throw new Error('Unable to copy obj! Its type isn\'t supported.');
+                }
+                var Alllayers = clone($scope.parlayers);
                 angular.extend($scope, {
+                    center: $scope.parcenter,
+                    layers: {
+                        baselayers: $scope.parlayers
+
+                    },
                     defaults: {
                         zoomControl: false
                     },
@@ -29,28 +62,29 @@
                         }
                     }
                 });
-                if ($scope.center == undefined) {
-                    angular.extend($scope, {
-                        center: $scope.center
-                    });
+           
+                // var test = {
+                //     luchtfoto: {
+                //         name: 'luchtfoto',
+                //         url: 'http://tile.informatievlaanderen.be/ws/raadpleegdiensten/tms/1.0.0/omwrgbmrvl@GoogleMapsVL/{z}/{x}/{y}.png',
+                //         type: 'xyz',
+                //         visible: true,
+                //         layerOptions: {
+                //             showOnSelector: true,
+                //             tms: true
+                //         }
+                //     }
+                // };
+                $scope.changeBaseLayer = function (key) {
+                    var thebaselayeritem = { themap: clone(Alllayers[key]) };
+                    $scope.layers.baselayers = null;
+                    thebaselayeritem.themap.visible = true;
+                    $scope.layers.baselayers = thebaselayeritem;
 
-                }
-                if ($scope.layers == undefined || $scope.layers.baselayers == undefined) {
-                    angular.extend($scope, {
-                        layers: {
-                            baselayers: {
-                                kaart: ourlayers.kaart,
-                                luchtfoto: ourlayers.luchtfoto
-                            },
-                            
-                        }
-                        
-                    });
-                }
-
+                };
             }
-        }
-    })
+        };
+    });
 })
     ();
 
@@ -66,10 +100,136 @@
     "<button type=button class=btn><i class=\"fa fa-download\"></i></button> </div> <div class=\"btn-group btn-group-vertical ll interactiebtns\"> <button type=button class=\"btn active\"><i class=\"fa fa-info\"></i></button>\n" +
     "<button type=button class=btn><i class=\"fa fa-mouse-pointer\"></i></button>\n" +
     "<button type=button class=btn><i class=\"fa fa-download\"></i></button> </div> <div class=\"ll zoekbalken\"> <input id=zoekbalk1 placeholder=\"Welke locatie of adres zoek je?\">\n" +
-    "<input class=invisible placeholder=\"Welke locatie of adres zoek je?\"> </div> <div class=\"ll btn-group ll kaarttypes\"> <button class=\"btn active\" ng-click=\"changeLayer('kaart')\">Kaart</button>\n" +
-    "<button class=btn ng-click=\"changeLayer('luchtfoto')\">Luchtfoto</button> </div> <div class=\"btn-group btn-group-vertical ll viewbtns\"> <button type=button class=btn><i class=\"fa fa-plus\"></i></button>\n" +
+    "<input class=invisible placeholder=\"Welke locatie of adres zoek je?\"> </div> <div class=\"ll btn-group ll kaarttypes\"> <button class=\"btn active\" ng-click=\"changeBaseLayer('kaart')\">Kaart</button>\n" +
+    "<button class=btn ng-click=\"changeBaseLayer('luchtfoto')\">Luchtfoto</button> </div> <div class=\"btn-group btn-group-vertical ll viewbtns\"> <button type=button class=btn><i class=\"fa fa-plus\"></i></button>\n" +
     "<button type=button class=btn><i class=\"fa fa-minus\"></i></button>\n" +
-    "<button type=button class=btn><i class=\"fa fa-crosshairs\"></i></button> </div> <div class=\"ll localiseerbtn\"> <button type=button class=btn><i class=\"fa fa-male\"></i></button> </div> </leaflet> </div> </div>"
+    "<button type=button class=btn><i class=\"fa fa-crosshairs\"></i></button> </div> <div class=\"ll localiseerbtn\"> <button type=button class=btn><i class=\"fa fa-male\"></i></button> </div> </leaflet> </div> </div> <style>.ll {\r" +
+    "\n" +
+    "        position: absolute;\r" +
+    "\n" +
+    "        z-index: 260;\r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "    \r" +
+    "\n" +
+    "    .interactiebtns {\r" +
+    "\n" +
+    "        left: 30px;\r" +
+    "\n" +
+    "        top: 100px;\r" +
+    "\n" +
+    "      \r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "    \r" +
+    "\n" +
+    "    .searchbtns {\r" +
+    "\n" +
+    "        left: 30px;\r" +
+    "\n" +
+    "        top: 40px;\r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "    \r" +
+    "\n" +
+    "    .zoekbalken {\r" +
+    "\n" +
+    "        left: 120px;\r" +
+    "\n" +
+    "        top: 40px;\r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "    \r" +
+    "\n" +
+    "    #zoekbalk1 {\r" +
+    "\n" +
+    "        width: 300px;\r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "    \r" +
+    "\n" +
+    "    .localiseerbtn {\r" +
+    "\n" +
+    "        right: 10px;\r" +
+    "\n" +
+    "        bottom: 20px;\r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "    \r" +
+    "\n" +
+    "    .viewbtns {\r" +
+    "\n" +
+    "        right: 10px;\r" +
+    "\n" +
+    "        bottom: 80px;\r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "\r" +
+    "\n" +
+    "    \r" +
+    "\n" +
+    "    .wrapper {\r" +
+    "\n" +
+    "        width: 100%;\r" +
+    "\n" +
+    "        height: 100%;\r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "    \r" +
+    "\n" +
+    "    button.btn.active {\r" +
+    "\n" +
+    "        background-color: dodgerblue;\r" +
+    "\n" +
+    "        color: white;\r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "    .leafletmap {\r" +
+    "\n" +
+    "        top: 0;\r" +
+    "\n" +
+    "        position: absolute;\r" +
+    "\n" +
+    "        left: 0;\r" +
+    "\n" +
+    "        right: 0;\r" +
+    "\n" +
+    "        bottom: 0;\r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "    .leaflet-left .leaflet-control-scale {\r" +
+    "\n" +
+    "        margin-left: 175px;\r" +
+    "\n" +
+    "        margin-bottom: 20px;\r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "    .kaarttypes {\r" +
+    "\n" +
+    "        left: 30px;\r" +
+    "\n" +
+    "        bottom:15px;\r" +
+    "\n" +
+    "    }\r" +
+    "\n" +
+    "    .kaarttypes > .btn \r" +
+    "\n" +
+    "    {\r" +
+    "\n" +
+    "        font-size: 0.5rem;\r" +
+    "\n" +
+    "    }</style>"
   );
 
 }]);
--- src/scripts/directives/tinkmap.js
+++ src/scripts/directives/tinkmap.js
@@ -7,19 +7,52 @@
     }
     module.directive('tinkMap', function () {
         return {
-            templateUrl: "templates/tinkmaptemplate.html",
-            scope: { 
-                layers: "=",
-                center:  "="
-              },
+            templateUrl: 'templates/tinkmaptemplate.html',
+            scope: {
+                parlayers: '=',
+                parcenter: '='
+            },
             controller: function ($scope) {
-                 var ourlayers = $scope.layers;
-             
+                function clone(obj) {
+                    var copy;
 
-                $scope.changeLayer = function (layername) {
-                    $scope.layers = ourlayers[layername];
-                };
+                    // Handle the 3 simple types, and null or undefined
+                    if (null == obj || "object" != typeof obj) return obj;
+
+                    // Handle Date
+                    if (obj instanceof Date) {
+                        copy = new Date();
+                        copy.setTime(obj.getTime());
+                        return copy;
+                    }
+
+                    // Handle Array
+                    if (obj instanceof Array) {
+                        copy = [];
+                        for (var i = 0, len = obj.length; i < len; i++) {
+                            copy[i] = clone(obj[i]);
+                        }
+                        return copy;
+                    }
+
+                    // Handle Object
+                    if (obj instanceof Object) {
+                        copy = {};
+                        for (var attr in obj) {
+                            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+                        }
+                        return copy;
+                    }
+
+                    throw new Error('Unable to copy obj! Its type isn\'t supported.');
+                }
+                var Alllayers = clone($scope.parlayers);
                 angular.extend($scope, {
+                    center: $scope.parcenter,
+                    layers: {
+                        baselayers: $scope.parlayers
+
+                    },
                     defaults: {
                         zoomControl: false
                     },
@@ -29,28 +62,29 @@
                         }
                     }
                 });
-                if ($scope.center == undefined) {
-                    angular.extend($scope, {
-                        center: $scope.center
-                    });
+           
+                // var test = {
+                //     luchtfoto: {
+                //         name: 'luchtfoto',
+                //         url: 'http://tile.informatievlaanderen.be/ws/raadpleegdiensten/tms/1.0.0/omwrgbmrvl@GoogleMapsVL/{z}/{x}/{y}.png',
+                //         type: 'xyz',
+                //         visible: true,
+                //         layerOptions: {
+                //             showOnSelector: true,
+                //             tms: true
+                //         }
+                //     }
+                // };
+                $scope.changeBaseLayer = function (key) {
+                    var thebaselayeritem = { themap: clone(Alllayers[key]) };
+                    $scope.layers.baselayers = null;
+                    thebaselayeritem.themap.visible = true;
+                    $scope.layers.baselayers = thebaselayeritem;
 
-                }
-                if ($scope.layers == undefined || $scope.layers.baselayers == undefined) {
-                    angular.extend($scope, {
-                        layers: {
-                            baselayers: {
-                                kaart: ourlayers.kaart,
-                                luchtfoto: ourlayers.luchtfoto
-                            },
-                            
-                        }
-                        
-                    });
-                }
-
+                };
             }
-        }
-    })
+        };
+    });
 })
     ();
 
