f32d3edce7392181abf4e7dd5f18494fe6220805
maxim <crabbemaxim@gmail.com>
2016-03-31 15:46:33 +0200

    added highlighting, todo unhighlight

--- src/scripts/search/controllers/searchSelectedController.js
+++ src/scripts/search/controllers/searchSelectedController.js
@@ -9,15 +9,28 @@
             vm.nextResult = null;
             vm.props = [];
             $scope.$watch(function() { return ResultsData.SelectedFeature; }, function(newVal, oldVal) {
-                if (newVal) {
-                    if (oldVal) {
-                        if (oldVal != newVal) {
-                            console.log("UNHIGHLIGHT: ");
-                            console.log(oldVal);
-                        }
+                if (oldVal) {
+                    if (oldVal != newVal) {
+                        console.log("UNHIGHLIGHT: ");
+                        console.log(oldVal);
+                        var tmplayer = newVal.mapItem._layers[Object.keys(newVal.mapItem._layers)[0]]
+                        console.log(tmplayer);
+                        tmplayer.setStyle({
+                            fillOpacity: 0
+                        });
                     }
+                }
+                if (newVal) {
                     console.log("HIGHLIGHT: ");
-                    console.log(newVal);
+                    // var layer = newVal.mapItem._layers.target;
+                    var tmplayer = newVal.mapItem._layers[Object.keys(newVal.mapItem._layers)[0]]
+                    console.log(tmplayer);
+                    tmplayer.setStyle({
+                        weight: 2,
+                        color: 'red',
+                        // dashArray: '',
+                        fillOpacity: 0.7
+                    });
 
                     vm.selectedResult = newVal;
                     var item = Object.getOwnPropertyNames(newVal.properties).map(k => ({ key: k, value: newVal.properties[k] }));
