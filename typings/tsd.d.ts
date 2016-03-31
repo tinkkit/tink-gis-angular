5878ceecd6eea027fd14778ae464a8f6545ed55b
Maxim Crabbé <crabbemaxim@gmail.com>
2016-03-31 03:26:16 +0200

    rebundled ts file

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,12 @@
 
+/// <reference path="angularjs/angular-animate.d.ts" />
+/// <reference path="angularjs/angular-component-router.d.ts" />
+/// <reference path="angularjs/angular-cookies.d.ts" />
+/// <reference path="angularjs/angular-mocks.d.ts" />
+/// <reference path="angularjs/angular-resource.d.ts" />
+/// <reference path="angularjs/angular-route.d.ts" />
+/// <reference path="angularjs/angular-sanitize.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="leaflet/leaflet.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
