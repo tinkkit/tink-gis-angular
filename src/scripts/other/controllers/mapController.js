96f979b0e6fcc4819d8a45a83129359116b6ed66
Maxim Crabbé <crabbemaxim@gmail.com>
2016-08-29 18:05:37 +0200

    working on leaflet identify on hidden layer for postzone

--- src/scripts/other/controllers/mapController.js
+++ src/scripts/other/controllers/mapController.js
@@ -1,5 +1,3 @@
-/// <reference path='../services/mapService.js' />
-
 'use strict';
 (function (module) {
     module = angular.module('tink.gis');
@@ -14,64 +12,111 @@
         vm.drawingType = MapData.DrawingType;
         vm.showMetenControls = false;
         vm.showDrawControls = false;
-        vm.zoekLoc = "";
-        var engine = new Bloodhound({
-            datumTokenizer: Bloodhound.tokenizers.whitespace,
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            local: []
-        });
-        var itemsdata = [];
-        $('#locatiezoek.typeahead').typeahead(null, {
-            name: 'name',
-            source: engine
+        vm.zoekLoc = '';
+        // var itemsdata = [];
+        var locatieMapData = L.esri.dynamicMapLayer({
+                        maxZoom: 19,
+                        minZoom: 0,
+                        url: 'http://geoint-a.antwerpen.be/arcgissql/rest/services/A_DA/Locaties/MapServer',
+                        opacity: 1,
+                        layers: 0,
+                        continuousWorld: true,
+                        useCors: true
+                    }).addTo(map);
+var suggestionfunc = function (item)
+{
+    var output = '<div>' + item.name;
+     if(item.attribute1value) {
+        output+= '<p>' + item.attribute1name + ': ' + item.attribute1value + '</p>'; 
+     }
+     
+     if(item.attribute2value) {
+        output+= '<p>' + item.attribute2name + ': ' + item.attribute2value + '</p>'; 
+     }
+     output += '<p>Laag: ' + item.layer + '</p></div>';
+     return output;
+}  
+        $('#locatiezoek.typeahead').typeahead({   
+            minLength: 3,
+  highlight: true,
+      classNames: {
+      open: 'is-open',
+      empty: 'is-empty',
+    //   cursor: 'is-active',
+    //   suggestion: 'Typeahead-suggestion',
+    //   selectable: 'Typeahead-selectable'
+    }
+}, {
+            async: true,
+            limit: 99,
+            display: 'displayField',
+              source: function(query, syncResults, asyncResults) {
+                  var prom = GISService.QuerySOLRLocatie(query);
+                    prom.then(function (data) {
+                    var arr = data.response.docs;
+                    // console.log('--------------------------------------------');
+                    // arr.forEach(x=>{
+                    //     console.log(x);
+                    //     x.displayField = x.name + ' (' + x.attribute1value + ')(' + x.layer + ')'; 
+                    // });
+                    // console.log('--------------------------------------------');
+                    asyncResults(arr);
+                    //         $('#locatiezoek.typeahead').typeahead('open');
+                  });
+         },
+           templates: {
+               suggestion: suggestionfunc,
+        notFound: ['<div class="empty-message"><b>Geen match gevonden</b></div>'],
+        empty: ['<div class="empty-message"><b>Zoek naar straten, POI en districten</b></div>']
+    }
+
         });
         $('.typeahead').bind('typeahead:select', function (ev, suggestion) {
-            var item = itemsdata.find(x => x.name == suggestion && x.layer == "WEGENREGISTER_STRAATAS_XY");
-            if (!item) {
-                item = itemsdata.find(x => x.name == suggestion);
+            if(suggestion.layer.toLowerCase() === 'postzone')
+            {
+                console.log('POSTZONEEEEEEE');
             }
+
             var cors = {
-                x: item.x,
-                y: item.y
-            }
+                x: suggestion.x,
+                y: suggestion.y
+            };
+            
             var xyWGS84 = HelperService.ConvertLambert72ToWSG84(cors);
             setViewAndPutDot(xyWGS84);
-            console.log('Selection: ' + item);
-        });
-        $('.typeahead').bind('typeahead:select', function (ev, suggestion) {
             console.log('Selection: ' + suggestion);
         });
-        $('#locatiezoek.typeahead').on('input', function (e) {
-            vm.zoekLoc = e.currentTarget.value;
-            var search = vm.zoekLoc;
-            if (search.length > 2) {
-                var prom = GISService.QuerySOLRLocatie(search);
-                prom.then(function (data) {
-                    var arr = data.response.docs;
-                    var names = arr.map(x => x.name);
-                    console.log(names);
-                    itemsdata = arr;
-                    engine.clear();
-                    engine.add(names);
-                    var promise = engine.initialize();
+        // $('#locatiezoek.typeahead').on('input', function (e) {
+        //     vm.zoekLoc = e.currentTarget.value;
+        //     var search = vm.zoekLoc;
+        //     if (search.length > 2) {
+        //         var prom = GISService.QuerySOLRLocatie(search);
+        //         prom.then(function (data) {
+        //             var arr = data.response.docs;
+        //             var names = arr.map(x => x.name);
+        //             console.log(names);
+        //             itemsdata = arr;
+        //             engine.clear();
+        //             engine.add(names);
+        //             var promise = engine.initialize();
 
-                    promise
-                        .done(function () {
-                            $('#locatiezoek.typeahead').typeahead('open');
-                            console.log('ready to go!');
-                        })
-                        .fail(function () {
-                            console.log('err, something went wrong :(');
-                        });
-                });
-            }
-            else {
-                console.log("engine clear");
-                engine.clear();
-                $('#locatiezoek.typeahead').typeahead('close');
+        //             promise
+        //                 .done(function () {
+        //                     $('#locatiezoek.typeahead').typeahead('open');
+        //                     console.log('ready to go!');
+        //                 })
+        //                 .fail(function () {
+        //                     console.log('err, something went wrong :(');
+        //                 });
+        //         });
+        //     }
+        //     else {
+        //         console.log("engine clear");
+        //         engine.clear();
+        //         $('#locatiezoek.typeahead').typeahead('close');
 
-            }
-        });
+        //     }
+        // });
         // vm.zoekLocChanged = function () {
         //     console.log("ZOEKLOCCHANGED");
         //     var search = vm.zoekLoc;
