f2e1def0b748e90eacbcdf4d826af6d4d261685e
Lieven VG <lievenvg@users.noreply.github.com>
2016-09-29 16:39:56 +0200

    Build

--- dist/tink-gis-angular.js
+++ dist/tink-gis-angular.js
@@ -3886,34 +3886,20 @@ L.drawLocal = {
     "<meta charset=utf-8>\n" +
     "<title>Street View side-by-side</title>\n" +
     "<style>\n" +
-    "html, body {\r" +
-    "\n" +
-    "        height: 100%;\r" +
-    "\n" +
-    "        margin: 0;\r" +
-    "\n" +
-    "        padding: 0;\r" +
-    "\n" +
-    "      }\r" +
-    "\n" +
-    "      #map,  {\r" +
-    "\n" +
-    "        float: left;\r" +
-    "\n" +
-    "        height: 0%;\r" +
-    "\n" +
-    "        width: 0%;\r" +
-    "\n" +
-    "      }\r" +
-    "\n" +
-    "       #pano {\r" +
-    "\n" +
-    "        float: left;\r" +
-    "\n" +
-    "        height: 100%;\r" +
-    "\n" +
-    "        width: 100%;\r" +
-    "\n" +
+    "html, body {\n" +
+    "        height: 100%;\n" +
+    "        margin: 0;\n" +
+    "        padding: 0;\n" +
+    "      }\n" +
+    "      #map,  {\n" +
+    "        float: left;\n" +
+    "        height: 0%;\n" +
+    "        width: 0%;\n" +
+    "      }\n" +
+    "       #pano {\n" +
+    "        float: left;\n" +
+    "        height: 100%;\n" +
+    "        width: 100%;\n" +
     "      }\n" +
     "</style>\n" +
     "</head>\n" +
@@ -3921,42 +3907,24 @@ L.drawLocal = {
     "<div id=map></div>\n" +
     "<div id=pano></div>\n" +
     "<script>\n" +
-    "function initialize() {\r" +
-    "\n" +
-    "        \r" +
-    "\n" +
-    "        var urlLat = parseFloat((location.search.split('lat=')[1]||'').split('&')[0]);\r" +
-    "\n" +
-    "        var urlLng = parseFloat((location.search.split('lng=')[1]||'').split('&')[0]);\r" +
-    "\n" +
-    "        var fenway = {lat:urlLat, lng: urlLng};\r" +
-    "\n" +
-    "        var map = new google.maps.Map(document.getElementById('map'), {\r" +
-    "\n" +
-    "          center: fenway,\r" +
-    "\n" +
-    "          zoom: 14\r" +
-    "\n" +
-    "        });\r" +
-    "\n" +
-    "        var panorama = new google.maps.StreetViewPanorama(\r" +
-    "\n" +
-    "            document.getElementById('pano'), {\r" +
-    "\n" +
-    "              position: fenway,\r" +
-    "\n" +
-    "              pov: {\r" +
-    "\n" +
-    "                heading: 34,\r" +
-    "\n" +
-    "                pitch: 10\r" +
-    "\n" +
-    "              }\r" +
-    "\n" +
-    "            });\r" +
-    "\n" +
-    "        map.setStreetView(panorama);\r" +
-    "\n" +
+    "function initialize() {\n" +
+    "        \n" +
+    "        var urlLat = parseFloat((location.search.split('lat=')[1]||'').split('&')[0]);\n" +
+    "        var urlLng = parseFloat((location.search.split('lng=')[1]||'').split('&')[0]);\n" +
+    "        var fenway = {lat:urlLat, lng: urlLng};\n" +
+    "        var map = new google.maps.Map(document.getElementById('map'), {\n" +
+    "          center: fenway,\n" +
+    "          zoom: 14\n" +
+    "        });\n" +
+    "        var panorama = new google.maps.StreetViewPanorama(\n" +
+    "            document.getElementById('pano'), {\n" +
+    "              position: fenway,\n" +
+    "              pov: {\n" +
+    "                heading: 34,\n" +
+    "                pitch: 10\n" +
+    "              }\n" +
+    "            });\n" +
+    "        map.setStreetView(panorama);\n" +
     "      }\n" +
     "</script>\n" +
     "<script async defer src=\"https://maps.googleapis.com/maps/api/js?callback=initialize\">\n" +
@@ -3967,8 +3935,8 @@ L.drawLocal = {
 
 
   $templateCache.put('templates/layermanagement/geoPuntTemplate.html',
-    "<div class=row>\n" +
-    "<div class=\"col-md-4 scrollable\">\n" +
+    "<div class=\"row margin-top\">\n" +
+    "<div class=\"col-md-4 scrollable-themes\">\n" +
     "<input class=searchbox ng-model=searchTerm ng-change=searchChanged() ng-model-options=\"{debounce: 500}\" placeholder=\"Geef een trefwoord of een url in\">\n" +
     "<input disabled value=https://geodata.antwerpen.be/arcgissql/services/P_SiK/Groeninventaris/MapServer/WMSServer>\n" +
     "<div ng-if=!searchIsUrl ng-repeat=\"theme in availableThemes\">\n" +
@@ -3980,7 +3948,7 @@ L.drawLocal = {
     "</div>\n" +
     "<tink-pagination ng-hide=\"numberofrecordsmatched <= 5\" tink-items-per-page-values=[5] tink-current-page=currentPage tink-change=pageChanged(page,perPage,next) tink-total-items=numberofrecordsmatched tink-items-per-page=recordsAPage></tink-pagination>\n" +
     "</div>\n" +
-    "<div class=\"col-md-8 scrollable\">\n" +
+    "<div class=col-md-8>\n" +
     "<div ng-if=searchIsUrl>\n" +
     "<button ng-click=laadUrl()>Laad url</button>\n" +
     "</div>\n" +
@@ -4012,7 +3980,7 @@ L.drawLocal = {
 
 
   $templateCache.put('templates/layermanagement/layersManagementTemplate.html',
-    "<div class=row>\n" +
+    "<div class=\"row margin-top\">\n" +
     "<div class=col-md-4>\n" +
     "<input class=searchbox ng-model=searchTerm ng-change=searchChanged() placeholder=\"Geef een trefwoord\">\n" +
     "<div ng-repeat=\"theme in availableThemes | filter:{name: searchTerm}\">\n" +
@@ -4025,7 +3993,7 @@ L.drawLocal = {
     "<preview-layer ng-if=copySelectedTheme theme=copySelectedTheme addorupdatefunc=AddOrUpdateTheme()>\n" +
     "</preview-layer>\n" +
     "</div>\n" +
-    "</div>"
+    "</div>\n"
   );
 
 
@@ -4045,37 +4013,50 @@ L.drawLocal = {
     "<input type=checkbox ng-model=\"lyrctrl.layer.enabled \" id={{lyrctrl.layer.name}}{{lyrctrl.layer.id}}>\n" +
     "<label for=\"{{lyrctrl.layer.name}}{{lyrctrl.layer.id}} \"> {{lyrctrl.layer.title | limitTo: 99}}</label>\n" +
     "</div>\n" +
-    "</div>"
+    "</div>\n"
   );
 
 
   $templateCache.put('templates/layermanagement/previewLayerTemplate.html',
-    "<div ng-show=\"theme !== null\">\n" +
+    "<div>\n" +
+    "<div class=row ng-show=\"theme !== null\">\n" +
+    "<div class=col-xs-12>\n" +
     "<button ng-if=\"theme.Added != false\" ng-click=addorupdatefunc()>Update</button>\n" +
     "<button ng-if=\"theme.Added != false\" ng-click=delTheme()>Delete</button>\n" +
+    "</div>\n" +
+    "</div>\n" +
+    "<div class=row>\n" +
+    "<div class=col-xs-12>\n" +
     "<p>{{theme.Description}}</p>\n" +
     "<p><small><a ng-href={{theme.CleanUrl}} target=_blank>Details</a></small></p>\n" +
-    "<div class=layercontroller-checkbox>\n" +
+    "</div>\n" +
+    "</div>\n" +
+    "<div class=row>\n" +
+    "<div class=\"col-xs-12 layercontroller-checkbox scrollable-preview\">\n" +
     "<input indeterminate-checkbox child-list=theme.AllLayers property=enabled type=checkbox ng-model=theme.enabled id={{theme.name}}>\n" +
     "<label for={{theme.name}}>{{theme.name}}</label>\n" +
     "<div ng-repeat=\"mainlayer in theme.Layers\">\n" +
-    "<tink-managementlayer layer=mainlayer>\n" +
-    "</tink-managementlayer>\n" +
+    "<tink-managementlayer layer=mainlayer></tink-managementlayer>\n" +
     "</div>\n" +
     "</div>\n" +
+    "</div>\n" +
+    "<div class=row>\n" +
+    "<div class=\"col-xs-12 margin-top\">\n" +
     "<button ng-if=\"theme.Added == false\" ng-click=addorupdatefunc()>Toevoegen</button>\n" +
-    "</div>"
+    "</div>\n" +
+    "</div>\n" +
+    "</div>\n"
   );
 
 
   $templateCache.put('templates/layermanagement/solrGISTemplate.html',
-    "<div class=\"row set-height\">\n" +
-    "<div class=\"col-md-4 set-height\">\n" +
-    "<div class=\"row set-height\">\n" +
+    "<div class=\"row margin-top\">\n" +
+    "<div class=col-md-4>\n" +
+    "<div class=row>\n" +
     "<div class=col-xs-12>\n" +
     "<input class=searchbox ng-model=searchTerm ng-change=searchChanged() ng-model-options=\"{debounce: 250}\" placeholder=\"Geef een trefwoord\">\n" +
     "</div>\n" +
-    "<div class=\"col-xs-12 scrollable\">\n" +
+    "<div class=\"col-xs-12 scrollable-themes\">\n" +
     "<div ng-repeat=\"theme in availableThemes\">\n" +
     "<div ng-click=solrThemeChanged(theme) class=greytext>\n" +
     "{{theme.name}}\n" +
@@ -4090,7 +4071,7 @@ L.drawLocal = {
     "</div>\n" +
     "</div>\n" +
     "</div>\n" +
-    "<div class=col-xs-12>\n" +
+    "<div class=\"col-xs-12 margin-top\">\n" +
     "<tink-pagination ng-hide=\"numberofrecordsmatched <= 5\" tink-current-page=currentPage tink-change=pageChanged(page,perPage,next) tink-total-items=numberofrecordsmatched tink-items-per-page=recordsAPage></tink-pagination>\n" +
     "</div>\n" +
     "</div>\n" +
@@ -4099,7 +4080,7 @@ L.drawLocal = {
     "<preview-layer ng-if=copySelectedTheme theme=copySelectedTheme addorupdatefunc=AddOrUpdateTheme()>\n" +
     "</preview-layer>\n" +
     "</div>\n" +
-    "</div>"
+    "</div>\n"
   );
 
 
