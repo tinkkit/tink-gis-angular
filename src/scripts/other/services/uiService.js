53832b459ed478774c76b0d7d3eb74b5d9d938fe
Maxim Crabbé <crabbemaxim@gmail.com>
2017-08-09 15:09:59 +0200

    added close left and right side function to uiservice

--- src/scripts/other/services/uiService.js
+++ src/scripts/other/services/uiService.js
@@ -1,25 +1,38 @@
 'use strict';
-(function () {
+(function() {
     var module;
     try {
         module = angular.module('tink.gis');
     } catch (e) {
         module = angular.module('tink.gis', ['tink.accordion', 'tink.tinkApi', 'ui.sortable', 'tink.modal', 'angular.filter', 'tink.pagination']); //'leaflet-directive'
     }
-    var service = function () {
+    var service = function() {
         var _service = {};
-        _service.OpenLeftSide = function () {
+        _service.OpenLeftSide = function() {
             let html = $('html');
             if (!html.hasClass('nav-left-open')) {
                 html.addClass('nav-left-open');
             }
         };
-        _service.OpenRightSide = function () {
+        _service.CloseLeftSide = function() {
+            let html = $('html');
+            if (html.hasClass('nav-left-open')) {
+                html.removeClass('nav-left-open');
+            }
+        };
+        _service.OpenRightSide = function() {
             let html = $('html');
             if (!html.hasClass('nav-right-open')) {
                 html.addClass('nav-right-open');
             }
         };
+        _service.CloseRightSide = function() {
+            let html = $('html');
+            if (html.hasClass('nav-right-open')) {
+                html.removeClass('nav-right-open');
+            }
+        };
+
         return _service;
     };
     module.factory('UIService', service);
