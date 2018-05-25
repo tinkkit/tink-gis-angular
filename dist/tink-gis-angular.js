59568ef8083e623635c4fa49a624fe5dac75af8c
Matthias Claes <matthias.claes@craxit.be>
2018-05-25 11:59:02 +0200

    fixed bug where child group layers wouldn't automatically open when they should

--- dist/tink-gis-angular.js
+++ dist/tink-gis-angular.js
@@ -1833,9 +1833,16 @@ var Scales = [250000, 200000, 150000, 100000, 50000, 25000, 20000, 15000, 12500,
         // vm.chkChanged = function () {
         //     $scope.$emit('layerCheckboxChangedEvent', $scope.layer); // stuur naar parent ofwel group ofwel theme
         // };
-        angular.forEach($scope.layer.Layers, function (value, key) {
+        angular.forEach($scope.layer.Layers, function (value) {
             if (value.enabled == true) {
                 $scope.showLayer = true;
+            }
+            if (value.Layers.length > 0) {
+                angular.forEach(value.Layers, function (childvalue) {
+                    if (childvalue.enabled == true) {
+                        $scope.showLayer = true;
+                    }
+                });
             }
         });
     });
