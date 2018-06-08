1e262997a08499bc9fbecb039f985065fe1e58ef
Matthias Claes <matthias.claes@craxit.be>
2018-06-08 12:14:57 +0200

    Fixed bug where a user would select a street, then clear the search and the last street would still be remembered. + small fix to the layer search, will now remove spaces, underscores and dashes

--- dist/tink-gis-angular.js
+++ dist/tink-gis-angular.js
@@ -1899,7 +1899,7 @@ var Scales = [250000, 200000, 150000, 100000, 50000, 25000, 20000, 15000, 12500,
             $scope.searchTerm = '';
         }();
         $scope.$on("searchChanged", function (event, searchTerm) {
-            $scope.searchTerm = searchTerm.replace('_', '').replace(' ', '');
+            $scope.searchTerm = searchTerm.replace(/ /g, '').replace(/_/g, '').replace(/-/g, '');
             if ($scope.searchTerm.length > 2) {
                 if ($scope.searchTerm != null && $scope.searchTerm != '') {
                     $scope.$parent.solrLoading = true;
@@ -2014,7 +2014,7 @@ var Scales = [250000, 200000, 150000, 100000, 50000, 25000, 20000, 15000, 12500,
                                 theme.layersCount = itemMetData.doclist.numFound;
                             }
                             itemMetData.doclist.docs.forEach(function (item) {
-                                if (item.titel[0].replace(' ', '').replace('_', '').toLowerCase().includes(searchterm.toLowerCase())) {
+                                if (item.titel[0].replace(/ /g, '').replace(/_/g, '').replace(/-/g, '').toLowerCase().includes(searchterm.toLowerCase())) {
                                     var layer = theme.layers.find(function (x) {
                                         return x.id == item.key;
                                     });
