b12e5778ecb5e89100ab744f366be7202cc538d0
Matthias Claes <matthias.claes@craxit.be>
2018-12-06 11:56:14 +0100

    Grunt build for hotfix

--- dist/tink-gis-angular.js
+++ dist/tink-gis-angular.js
@@ -4883,7 +4883,9 @@ L.control.typeahead = function (args) {
                         case ActiveInteractieButton.SELECT:
 
                             if (MapData.DrawingType != DrawingOption.GEEN && MapData.ExtendedType == null) {
-                                MapData.CleanMap();
+                                if (MapData.DrawingType != DrawingOption.VIERKANT) {
+                                    MapData.CleanMap();
+                                }
                                 MapData.CleanSearch();
                             }
                             if (MapData.DrawingType === DrawingOption.NIETS) {
