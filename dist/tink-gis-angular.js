552c7620d5fb5975d39a24ff1c52cefd988c5a02
Matthias Claes <matthias.claes@craxit.be>
2019-04-11 13:31:26 +0200

    chore: grunt build

--- dist/tink-gis-angular.js
+++ dist/tink-gis-angular.js
@@ -7996,7 +7996,7 @@ L.drawLocal = {
     "<select ng-model=operation.operator ng-options=\"x for x in operators\" ng-change=updateOperation($index)></select>\n" +
     "</div>\n" +
     "<div class=\"form-group col-xs-4\">\n" +
-    "<input value={{operation.value}} placeholder=Waarde ng-model=operation.value ng-change=updateOperation($index) ng-click=valueChanged($index) ng-model-options=\"{ debounce: 500 }\" id=input_waarde class=twitter-typeahead>\n" +
+    "<input value={{operation.value}} placeholder=Waarde ng-model=operation.value ng-change=updateOperation($index) ng-click=valueChanged($index) ng-model-options=\"{ debounce: 500 }\" id=input_waarde>\n" +
     "</div>\n" +
     "<div class=\"form-group col-xs-2\">\n" +
     "<button type=button class=up ng-show=!$first ng-click=up($index)></button>\n" +
